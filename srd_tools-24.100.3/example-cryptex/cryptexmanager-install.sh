59483ac5bd240d5ba91ba9ca5df621b9677f9477
David Hoyt <xss@xss.cx>
2022-03-24 07:29:09 -0400

    Update cryptexmanager-install.sh

--- srd_tools-24.100.3/example-cryptex/cryptexmanager-install.sh
+++ srd_tools-24.100.3/example-cryptex/cryptexmanager-install.sh
@@ -3,6 +3,8 @@ echo "umount com.example.cryptex"
 cryptexctl uninstall com.example.cryptex
 echo "Getting DMG...."
 wget https://github.com/xsscx/srd/raw/main/dmg/srd-universal-cryptex.dmg
+echo "running.. hdiutil attach srd-universal-cryptex.dmg"
+hdiutil attach srd-universal-cryptex.dmg
 echo "Installing a Cryptex with CryptexManager\n\n Using CryptexManager create -i com.example.cryptex -v 1.3.3.7  srd-universal-cryptex.dmg /Volumes/com.example.cryptex.dstroot /tmp/cptx\n\n"
 CryptexManager create -i com.example.cryptex -v 1.3.3.7  srd-universal-cryptex.dmg /Volumes/com.example.cryptex.dstroot /tmp/cptx
 CryptexManager install /tmp/cptx
