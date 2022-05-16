4ae2162d87f0b3bbc6dc01f267162933d12585ea
David Hoyt <xss@xss.cx>
2022-05-16 07:04:00 -0400

    Poor Man's Fixup for Dropbear configure.ac for SRD
    #!/bin/sh
    echo "Poor Man's Fixup for Dropbear configure.ac for SRD"
    rm src/dropbear/dropbear
    rm src/dropbear/dropbear-src/*.o
    wget https://raw.githubusercontent.com/xsscx/srd/main/srd_tools-24.100.3/example-cryptex/src/dropbear/configure.ac
    mv configure.ac src/dropbear/dropbear-src
    make
    echo "Done here with the dropbear fixup, now back to installing..."

---
+++ srd_tools-24.100.3/example-cryptex/dropbear-fixup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+echo "Poor Man's Fixup for Dropbear configure.ac for SRD"
+rm src/dropbear/dropbear
+rm src/dropbear/dropbear-src/*.o
+wget https://raw.githubusercontent.com/xsscx/srd/main/srd_tools-24.100.3/example-cryptex/src/dropbear/configure.ac
+mv configure.ac src/dropbear/dropbear-src
+make
+echo "Done here with the dropbear fixup, now back to installing..."
